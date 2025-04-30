bce1f474abdbed0ea72a7083144b58b521fff4f2
must-Zeus0036 <mustafa.al-bayati0036@stud.hkr.se>
2025-04-30 12:14:50 +0200

    change port

--- server/index.js
+++ server/index.js
@@ -46,5 +46,5 @@ app.use((req, res, next) => {
   })
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
